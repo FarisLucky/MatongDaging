fede7041f46979c448b80428e902ce19f2e3fb08
Ali Rahmatullah <alirahmatullah68@gmail.com>
2019-05-03 12:50:20 +0700

    kategori kelompok
    CRUD Kategori kelompok, ini kategori kelompok rek, kmarin salah membuat brennya aku

---
+++ application/views/kategori_kelompok/v_tambah_kategori.php
@@ -0,0 +1,8 @@
+<form action="<?php echo base_url(). 'index.php/kategori_kelompok/tambah_aksi'; ?>" method="post">
+                    <div class="form-group">
+                      <label for="exampleInputName1">nama_kategori</label>
+                      <input type="text" name = "nama_kategori" class="form-control" id="exampleInputName1" placeholder="">
+                    </div>
+                    <td></td>
+                    <td><input type="submit" value="Tambah"></td>
+                </form>
