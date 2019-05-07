fbc728afea928cc5bd391d7c6c98b1b83e3a682b
Ali Rahmatullah <alirahmatullah68@gmail.com>
2019-05-07 08:19:35 +0700

    blok/kategori item
    Crud kategori item, dimana ketika tambah id_user dan id_kategori defaultnya 1 dan 3, untuk default yang diambil tersebut tidak menggunakan season

---
+++ application/views/kategori_item/v_edit_item.php
@@ -0,0 +1,11 @@
+<?php foreach($kategori_item as $k){ ?>
+<form action="<?php echo base_url(). 'index.php/kategori_item/update'; ?>" method="post">
+                    <div class="form-group">
+                      <label for="exampleInputName1">nama_kelompok</label>
+                        <input type="hidden" name = "id_kelompok" value = "<?php echo $k->id_kelompok ?>"class="form-control" id="exampleInputName1" placeholder="">
+                      <input type="text" name = "nama_kelompok" value = "<?php echo $k->nama_kelompok ?>"class="form-control" id="exampleInputName1" placeholder="">
+                    </div>
+                    <td></td>
+                    <td><input type="submit" value="Simpan"></td>
+                </form>
+                <?php } ?>
---
+++ application/views/kategori_item/v_tambah_item.php
@@ -0,0 +1,9 @@
+<form action="<?php echo base_url(). 'index.php/kategori_item/tambah_aksi'; ?>" method="post">
+                    <div class="form-group">
+                      <label for="exampleInputName1">nama_kelompok</label>
+                      <input type="text" name = "nama_kelompok" class="form-control" id="exampleInputName1" placeholder="">
+
+                    </div>
+                    <td></td>
+                    <td><input type="submit" value="Tambah"></td>
+                </form>
